9131dd60c9dfbbfa6aa12438141fad1089436475
buty4649 <buty4649@gmail.com>
2025-04-14 20:02:34 +0900

    feat: add version flag to display version information

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/pepabo/onecli/cmd"
+import (
+	"fmt"
+	"os"
+
+	"github.com/pepabo/onecli/cmd"
+)
+
+var version string
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "--version" {
+		fmt.Printf("onecli version %s\n", version)
+		return
+	}
 	cmd.Execute()
 }
