99565b9a2147df00b7b27ed154e8261f0e4df241
buty4649 <buty4649@gmail.com>
2025-06-18 15:33:35 +0900

    refactor: remove --version handling from main.go (handled by cobra subcommand)

--- main.go
+++ main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/pepabo/onecli/cmd"
 )
 
-var version string
-
 func main() {
-	if len(os.Args) > 1 && os.Args[1] == "--version" {
-		fmt.Printf("onecli version %s\n", version)
-		return
-	}
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
